b9d6a72cdc980202305a3b3043b08ced2819511f
Mikhail Gaile <michael2ga3@gmail.com>
2024-03-10 10:04:11 +0300

    feat: add autoimport new commands

--- agent/commands/__init__.py
+++ agent/commands/__init__.py
@@ -1,9 +1,17 @@
-from .command import BaseCommand
-from .hash import Hash
-from .start import Start
-from .help import Help
-from .wordlists import Wordlists
-from .rules import Rules
-from .register_command import command_registry
+import os
+import pkgutil
+import importlib
 
-__all__ = ["BaseCommand", "Hash", "Start", "Help", "Wordlists", "Rules", "command_registry"]
+__all__ = []
+
+current_dir = os.path.dirname(__file__)
+module_names = [
+    name for _, name, _ in pkgutil.iter_modules([current_dir]) if name != "__init__"
+]
+for module_name in module_names:
+    module = importlib.import_module(f".{module_name}", package=__name__)
+
+    for attribute_name in dir(module):
+        if not attribute_name.startswith("_"):
+            globals()[attribute_name] = getattr(module, attribute_name)
+            __all__.append(attribute_name)
