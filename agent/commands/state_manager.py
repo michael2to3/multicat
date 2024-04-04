63d9ccb451f73c5cc2fd8c4b8661af94e798205e
Mikhail Gaile <michael2ga3@gmail.com>
2024-04-04 09:57:12 +0300

    ref: use lazzy logger

--- agent/commands/state_manager.py
+++ agent/commands/state_manager.py
@@ -19,20 +19,16 @@ class StateManager:
     def add_command(cls, user_id: str, command: str):
         user_state = cls._get_or_create_user_state(user_id)
         user_state.active_command = command
-        logger.info(f"Command added for user {user_id} with command {command}")
+        logger.info("Command added for user %s with command %s", user_id, command)
 
     @classmethod
     def remove_command(cls, user_id: str):
         user_state = cls._get_or_create_user_state(user_id)
         if user_state.active_command:
-            logger.info(
-                f"Command '{user_state.active_command}' removed for user {user_id}"
-            )
+            logger.info("Command '%s' removed for user %s", user_state.active_command, user_id)
             user_state.active_command = ""
         else:
-            logger.info(
-                f"Tried to remove command for user {user_id}, but none was found"
-            )
+            logger.info("Tried to remove command for user %s", user_id)
 
     @classmethod
     def get_command_message_id(cls, user_id: str) -> str:
@@ -48,7 +44,7 @@ class StateManager:
     def set_last_message_text(cls, user_id: str, text: str):
         user_state = cls._get_or_create_user_state(user_id)
         user_state.last_message_text = text
-        logger.info(f"Last message text set for user {user_id}")
+        logger.info("Last message text set for user %s: %s", user_id, text)
 
     @classmethod
     def has_last_message_text(cls, user_id: str, text: str) -> bool:
@@ -59,7 +55,7 @@ class StateManager:
     def set_last_execution(cls, user_id: str, execution_time: datetime):
         user_state = cls._get_or_create_user_state(user_id)
         user_state.last_execution = execution_time
-        logger.info(f"Last execution time set for user {user_id}: {execution_time}")
+        logger.info("Last execution time set for user %s: %s", user_id, execution_time)
 
     @classmethod
     def get_last_execution(cls, user_id: str) -> datetime:
@@ -70,15 +66,15 @@ class StateManager:
     def reset_state(cls, user_id: str):
         if user_id in cls._states:
             del cls._states[user_id]
-            logger.info(f"State reset for user {user_id}")
+            logger.info("State reset for user %s", user_id)
         else:
-            logger.info(f"No state found to reset for user {user_id}")
+            logger.info("No state found to reset for user %s", user_id)
 
     @classmethod
     def remove_last_message_text(cls, user_id: str):
         user_state = cls._get_or_create_user_state(user_id)
         user_state.last_message_text = ""
-        logger.info(f"Last message text removed for user {user_id}")
+        logger.info("Last message text removed for user %s", user_id)
 
     @classmethod
     def _get_or_create_user_state(cls, user_id: str) -> UserState:
