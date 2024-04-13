7ff5002d08c71f35e2dc1fc1cd24e1e826bd3b93
Mikhail Gaile <michael2ga3@gmail.com>
2024-04-13 22:32:11 +0300

    ref: use protected var in step deleter

--- server/steps/deleter.py
+++ server/steps/deleter.py
@@ -10,20 +10,20 @@ logger = logging.getLogger(__name__)
 
 class StepDeleter:
     def __init__(self, user_id: str, session: scoped_session):
-        self.db_helper = DatabaseHelper(session)
-        self.session = session
-        self.user_id = user_id
+        self._dbh = DatabaseHelper(session)
+        self._session = session
+        self._user_id = user_id
 
     def delete_step(self, step_name: int):
-        user = self.db_helper.get_or_create_user(self.user_id)
+        user = self._dbh.get_or_create_user(self._user_id)
         step = self._get_step(user.id, step_name)
         if not step:
             raise ValueError("Step not found.")
-        self.session.delete(step)
+        self._session.delete(step)
 
     def _get_step(self, user_id, step_name):
         return (
-            self.session.query(Step)
+            self._session.query(Step)
             .filter(Step.name == step_name, Step.user_id == user_id)
             .first()
         )
